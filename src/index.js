a3b2c5718e206d1c34631c463a19a4ab4841cdfd
Yarkin13 <stas20072008@yandex.ru>
2020-03-08 11:50:37 +0400

    docs: task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  if (array.length==0) {
+    return 0;
+  }
+  return Math.min.apply( Math, array );
+
+
+};
+
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  if (array.length==0) {
+    return 0;
+  }
+  return Math.max.apply( Math, array );
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  if (array.length==0) {
+    return 0;
+  }
+  let sum=0;
+  for (let i=0; i<array.length; i++) {
+    sum+=array[i];
+  }
+  return sum/array.length;
+ 
 }
